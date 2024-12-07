3033a6360bc85419d2ce85a37edc5627107c0c71
Jpalflo <jabossdesigns@gmail.com>
2024-12-07 19:12:16 +0100

    Change Scene
    Scripts for Exit and Start Buttons plus textured animations

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("OutdoorsScene");
+    }
+
+    public void ExitScene()
+    {
+        Application.Quit();
+    }
+}
