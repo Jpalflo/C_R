2c2da5f6200b65109036c5670149542b5a9b73ca
Jpalflo <jabossdesigns@gmail.com>
2024-12-09 23:48:03 +0100

    UX completed
    Todo relacionado con experiencia de usuario está conforme, listo y aplicado

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,12 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene()
+    private Coroutine coroutine;
+
+    public void LoadScene(int sceneIndex) //Change the scene game
     {
+
+        if (coroutine == null)
+        {
+            coroutine = StartCoroutine(LoadSceneWithTime(sceneIndex));
+        }
+        //AudioManager.Instance?.PlayButtonSound();
         SceneManager.LoadScene("OutdoorsScene");
+
     }
 
-    public void ExitScene()
+    IEnumerator LoadSceneWithTime(int sceneIndex) //Change scene after 0.25 seconds :)
+    {
+
+        yield return new WaitForSeconds(0.25f);
+        coroutine = null;
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void ExitScene() //Quit the game
     {
         //AudioManager.Instance?.PlayButtonSound();
 
@@ -18,8 +35,33 @@ public class SceneLoader : MonoBehaviour
             // Si estamos en el editor, detén el juego
             UnityEditor.EditorApplication.isPlaying = false;
         #else
-        // Si es una compilación, cierra la aplicación
+        // If is a compilation, close the app
         Application.Quit();
         #endif
     }
+
+    public void Restart() //Restart the scene
+    {
+        //AudioManager.Instance?.PlayButtonSound();
+
+        SceneManager.LoadScene("OutdoorsScene");
+    }
+
+    public void OptionScene() //You can go to Option Scene, where could you change the volume with sliders
+    {
+        //AudioManager.Instance?.PlayButtonSound();
+
+        SceneManager.LoadScene("OptionScene");
+    }
+
+    public void MenuScene()
+    {
+        //AudioManager.Instance?.PlayButtonSound();
+
+        //GameManager.Instance.PlayerPoints = 0;
+        //go to menu level
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    
 }
